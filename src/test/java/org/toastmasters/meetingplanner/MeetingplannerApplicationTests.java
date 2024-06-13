6e14f3fe811f382b6fe4faba03c8bd7df04d76bb
haakoaho <hakon.hoff@outlook.com>
2024-06-13 20:08:06 +0200

    Initial commit

---
+++ src/test/java/org/toastmasters/meetingplanner/MeetingplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.toastmasters.meetingplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
