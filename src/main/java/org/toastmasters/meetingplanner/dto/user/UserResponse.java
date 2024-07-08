9a4bde950296d7f43a405abb2d3c28c7992afee6
haakoaho <hakon.hoff@outlook.com>
2024-07-08 21:41:26 +0200

    Update UserResponse.java

--- src/main/java/org/toastmasters/meetingplanner/dto/user/UserResponse.java
+++ src/main/java/org/toastmasters/meetingplanner/dto/user/UserResponse.java
@@ -1,8 +1,11 @@
 package org.toastmasters.meetingplanner.dto.user;
 
-public record UserResponse(String name, String email, String phoneNumber, Long id) {
+import java.util.Map;
+
+public record UserResponse(String name, String email, String phoneNumber, Long id, Map<String, Object> meetingHistory,
+                           boolean photoConsent) {
 
     public static UserResponse fromUser(User user) {
-        return new UserResponse(user.getName(), user.getEmail(), user.getPhoneNumber(), user.getId());
+        return new UserResponse(user.getName(), user.getEmail(), user.getPhoneNumber(), user.getId(), user.getMeetingHistory(), user.isPhotoConsent());
     }
 }
