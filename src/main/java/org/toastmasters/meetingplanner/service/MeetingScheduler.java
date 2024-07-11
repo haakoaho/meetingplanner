1a7881d215575497b773f5cf08f31dddbd468c25
haakoaho <hakon.hoff@outlook.com>
2024-07-11 22:21:44 +0200

    Fix and enable scheduling for realz

--- src/main/java/org/toastmasters/meetingplanner/service/MeetingScheduler.java
+++ src/main/java/org/toastmasters/meetingplanner/service/MeetingScheduler.java
@@ -1,9 +1,11 @@
 package org.toastmasters.meetingplanner.service;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableScheduling
 public class MeetingScheduler {
 
     private final MeetingService meetingService;
@@ -12,8 +14,9 @@ public class MeetingScheduler {
         this.meetingService = meetingService;
     }
 
-    @Scheduled(cron = "0 0 2 1/1 * ? *")
+    @Scheduled(cron = "0 0 2 1/1 * *")
     public void runJob() throws Exception {
         meetingService.archiveMeeting();
     }
+
 }
